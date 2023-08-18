abbd19277de1a580c6e89d2be0663e5633717da9
mitsu1119 <hachimitsu1119@gmail.com>
2023-08-18 15:01:12 +0900

    他の演算

--- src/ring/integer_ring.rs
+++ src/ring/integer_ring.rs
@@ -9,7 +9,7 @@ use crate::util::set::Set;
 // ----------------------------------------------------------------
 // element of Integer Ring
 // ----------------------------------------------------------------
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct IntegerRingElement<'a> {
     val: Integer,
     parent: &'a IntegerRing
@@ -33,6 +33,65 @@ impl IntegerRingElement<'_> {
 // ----------------------------------------------------------------
 // Arithmetic Operators
 // ----------------------------------------------------------------
+impl Add for IntegerRingElement<'_> {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        let mut res = self;
+        res += other;
+        res
+    }
+}
+
+impl Sub for IntegerRingElement<'_> {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        let mut res = self;
+        res -= other;
+        res
+    }
+}
+
+impl Mul for IntegerRingElement<'_> {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self::Output {
+        let mut res = self;
+        res *= other;
+        res
+    }
+}
+
+impl Rem for IntegerRingElement<'_> {
+    type Output = Self;
+    fn rem(self, other: Self) -> Self::Output {
+        let mut res = self;
+        res %= other;
+        res
+    }
+}
+
+impl AddAssign for IntegerRingElement<'_> {
+    fn add_assign(&mut self, other: Self) {
+        self.val += other.val;
+    }
+}
+
+impl SubAssign for IntegerRingElement<'_> {
+    fn sub_assign(&mut self, other: Self) {
+        self.val -= other.val;
+    }
+}
+
+impl MulAssign for IntegerRingElement<'_> {
+    fn mul_assign(&mut self, other: Self) {
+        self.val *= other.val;
+    }
+}
+
+impl RemAssign for IntegerRingElement<'_> {
+    fn rem_assign(&mut self, other: Self) {
+        self.val %= other.val;
+    }
+}
 
 // ----------------------------------------------------------------
 // Set of Integer Ring
@@ -76,10 +135,23 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let x = ZZ.lift(5);
+        let mut x = ZZ.lift(5);
         println!("{:?}", x);
         assert!(!ZZ.from_str("63881801352479295820993181514863074496724272670065922597057931030213707690709").is_prime());
         assert!(ZZ.from_str("12513184754843391318297613509216180345616625665202041567883117414712490368118894860874737593058204662888282042487382831063055422314262328170908247278561361").is_prime());
+
+        x += ZZ.lift(3);
+        assert_eq!(x, ZZ.lift(8));
+
+        let y = x + ZZ.lift(2);
+        assert_eq!(y, ZZ.lift(10));
+
+        assert_eq!(ZZ.lift(2) + ZZ.lift(5), ZZ.lift(7));
+        assert_eq!(ZZ.lift(2) - ZZ.lift(5), ZZ.lift(-3));
+        assert_eq!(ZZ.lift(5) - ZZ.lift(2), ZZ.lift(3));
+        assert_eq!(ZZ.lift(5) * ZZ.lift(2), ZZ.lift(10));
+        assert_eq!(ZZ.lift(10) % ZZ.lift(6), ZZ.lift(4));
+        assert_eq!(ZZ.lift(-10) % ZZ.lift(6), ZZ.lift(-4));
     }
 
     #[test]
