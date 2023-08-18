638f685ab5c8c56c1255b182e6c4e5bf38fde188
mitsu1119 <hachimitsu1119@gmail.com>
2023-08-18 18:31:59 +0900

    Set と Element の関係を有限体に実装

--- src/field/finite_field.rs
+++ src/field/finite_field.rs
@@ -1,5 +1,29 @@
 use std::fmt;
+use crate::util::element::Element;
+use crate::util::set::Set;
 use crate::ring::integer_ring::*;
+
+// ----------------------------------------------------------------
+// element of Integer Ring
+// ----------------------------------------------------------------
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct FiniteFieldElement<'a> {
+    val: IntegerRingElement<'a>,
+    parent: &'a FiniteField<'a>
+}
+
+impl fmt::Display for FiniteFieldElement<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.val)
+    }
+}
+
+impl<'a> Element for FiniteFieldElement<'a> {
+    type Parent = FiniteField<'a>;
+    fn parent(&self) -> &'a Self::Parent {
+        self.parent
+    }
+}
 
 // ----------------------------------------------------------------
 // Set of Finite Field
@@ -15,12 +39,20 @@ impl fmt::Display for FiniteField<'_> {
     }
 }
 
+impl<'a> Set<'a> for FiniteField<'_> {
+    type Child = FiniteFieldElement<'a>;
+}
+
 impl FiniteField<'_> {
     fn new(order: IntegerRingElement) -> FiniteField {
-        // TODO: 位数が素数冪の有限体
+        // TODO: finite field that order is prime power
         if !order.is_prime() { panic!("The order of FiniteField must be prime."); }
         FiniteField { order: order }
     }
+}
+
+fn GF(order: IntegerRingElement) -> FiniteField {
+    FiniteField::new(order)
 }
 
 // ----------------------------------------------------------------
@@ -33,8 +65,10 @@ mod tests {
     #[test]
     fn it_words() {
         let Fp = FiniteField::new(ZZ.lift(5));
+        let FF = GF(ZZ.lift(5));
         println!("{:?}", Fp);
         println!("{}", Fp);
+        assert_eq!(Fp, FF);
     }
 
     #[test]
